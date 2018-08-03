a0be57d921a22e4d55b8753a2992012060d5cc68
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-03 20:48:12 -0300

    primeiro commit

---
+++ 01.Fiap.HelloWorld.Console/01.Fiap.HelloWorld.Console.UI/Model/Lancha.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.Console.UI.Model
+{
+    class Lancha : Veiculo
+
+        //Identar Ctrl k d
+
+    {
+
+        public bool BoiaSalvavida { get; set; }
+
+    }
+}
---
+++ 01.Fiap.HelloWorld.Console/01.Fiap.HelloWorld.Console.UI/Program.cs
@@ -0,0 +1,33 @@
+﻿using _01.Fiap.HelloWorld.Console.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.Console.UI
+{
+    class Program
+    {
+
+        //Ctrl .
+
+        static void Main(string[] args)
+        {
+            //Instanciar um carro
+            Carro carro = new Carro();
+            carro.Ano = 2010; //set
+            carro.ArCondicionado = false;
+
+            System.Console.WriteLine(carro.Ano); //get
+
+            //Instanciar a Lancha
+            var lancha = new Lancha()
+            {
+                BoiaSalvavida = true,
+                Ano = 2014
+            };
+
+        }
+    }
+}
