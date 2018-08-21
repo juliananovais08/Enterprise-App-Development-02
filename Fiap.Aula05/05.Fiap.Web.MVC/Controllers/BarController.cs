f0139fbecdefbb28f31e6f44bec6991697f6275b
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-20 22:55:23 -0300

    Aula05-exercicio

---
+++ Fiap.Aula05/05.Fiap.Web.MVC/Controllers/BarController.cs
@@ -0,0 +1,40 @@
+﻿using _05.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _05.Fiap.Web.MVC.Controllers
+{
+    public class BarController : Controller
+    {
+
+        private static List<Bar> _lista = new List<Bar>();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            List<string> cidades = new List<string>();
+            cidades.Add("São Paulo");
+            cidades.Add("Rio de Janeiro");
+            cidades.Add("Curitiba");
+            // Enviar apara a pagina a lista de cidades para o select
+            ViewBag.listaCidades = new SelectList(cidades);
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            return View(_lista);
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Bar bar)
+        {
+            _lista.Add(bar);
+            TempData["msg"] = "Bar Cadastrado"; //Exibe na prox página após o redirect
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
