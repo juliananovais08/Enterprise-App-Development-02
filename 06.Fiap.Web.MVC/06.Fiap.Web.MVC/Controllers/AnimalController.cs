8555f54fc008f92f345ca8021c2c297c5322fde6
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-31 20:46:54 -0300

    final do CRUD - remove e criação da funcionalidade pesquisar

--- 06.Fiap.Web.MVC/06.Fiap.Web.MVC/Controllers/AnimalController.cs
+++ 06.Fiap.Web.MVC/06.Fiap.Web.MVC/Controllers/AnimalController.cs
@@ -40,11 +40,12 @@ namespace _06.Fiap.Web.MVC.Controllers
             return View(animal);
         }
 
+        [HttpPost]
         public ActionResult Alterar(Animal animal)
         {
             _contex.Entry(animal).State = System.Data.Entity.EntityState.Modified;
             _contex.SaveChanges();
-            TempData["msg"] = "Animal atualizado";
+            TempData["msg"] = "Animal atualizado!";
             return RedirectToAction("listar");
         }
 
@@ -55,6 +56,32 @@ namespace _06.Fiap.Web.MVC.Controllers
             _contex.SaveChanges();
             TempData["msg"] = "Cadastrado com sucesso!";
             return RedirectToAction("Cadastrar");
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(int id)
+        {
+            //Busca o animal pelo codigo
+            var animal = _contex.Animais.Find(id);
+            //Remove o animal pelo banco
+            _contex.Animais.Remove(animal);
+            //Commit
+            _contex.SaveChanges();
+            //Mensagem de sucesso
+            TempData["msg"] = "Animal excluido";
+            //Retorna para a lista
+            return RedirectToAction("listar");
+        }
+
+        [HttpGet]
+        public ActionResult Pesquisar(string nome)
+        {
+            //Pesquisar animais pelo nome
+            var lista = _contex.Animais.Where(a => a.Nome.Contains(nome)).ToList();
+            //retornar para a página Listar.cshtml
+            //com a lista de animais "filtrado"
+            return View("Listar", lista);
+
         }
     }
 }
